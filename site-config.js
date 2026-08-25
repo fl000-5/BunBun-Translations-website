d095122aacce0ab427d2458d50f77a34e225a503
𝕱𝖑𝖔𝖔𝖔 <kuba.ociepa7@gmail.com>
2026-08-25 18:50:15 +0200

    Update site-config.js

--- site-config.js
+++ site-config.js
@@ -1,3 +1,3 @@
 // Publiczna konfiguracja frontendu. To nie jest miejsce na tokeny ani sekrety.
 // Gdy backend bota stoi osobno, wpisz tu jego adres, np. "https://bunbun-api.onrender.com".
-window.BUNBUN_API_BASE = "https://florists-strings-penetration-tax.trycloudflare.com";
+window.BUNBUN_API_BASE = "https://excited-desirable-bacon-employ.trycloudflare.com";
